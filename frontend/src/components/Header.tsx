a4e7c55e52396ae9bad7259d3c487eedcd46c745
gsunil1996 <gajulasunilkumar123@gmail.com>
2024-04-29 19:17:56 +0530

    update

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,28 @@
+
+// import MobileNav from "./MobileNav";
+// import MainNav from "./MainNav";
+
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <div className="border-b-2 border-b-orange-500 py-6">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link
+          href="/"
+          className="text-3xl font-bold tracking-tight text-orange-500"
+        >
+          MernEats.com
+        </Link>
+        {/* <div className="md:hidden">
+          <MobileNav />
+        </div>
+        <div className="hidden md:block">
+          <MainNav />
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
